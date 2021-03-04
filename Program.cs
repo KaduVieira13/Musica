537e44e8c262ec9cef15a9873472899aa0e2f248
carlos eduardo <carloseduardo_alencar@outlook.com>
2021-03-04 09:20:28 -0300

    Adiciona letra da musica

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace Musica
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("Laura Soguellis");
             Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine(" Seguro estou nos braços \n Daquele que nunca me deixou.");
+            Console.WriteLine(" Seu amor perfeito sempre esteve \n Repousando em mim.");
+            Console.WriteLine();
+            Console.WriteLine(" | Pré-Refrão |");
+            Console.WriteLine();
+            Console.WriteLine(" E se eu passar pelo vale \n Acharei conforto em seu amor");
+            Console.WriteLine(" Pois eu sei que és aquele \n Que me guarda, me guarda.");
+            Console.WriteLine();
+            Console.WriteLine(" | Refrão |");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" Em teus braços é o meu descanso \n Em teus braços é o meu descanso");
+            Console.WriteLine(" Em teus braços é o meu descanso \n Em teus braços é o meu descanso.");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine(" Recaio em tua graça \n Recaio de novo em tuas mãos.");
+            Console.WriteLine(" Recaio em tua graça \n Recaio de novo em tuas mãos.");
+
+
             
         }
     }
